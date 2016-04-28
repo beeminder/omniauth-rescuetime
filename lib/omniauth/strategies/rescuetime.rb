6a874fdabad185dd04ae94e767e2f1d441a2463d
Bethany Marie Soule <bsoule@gmail.com>
2016-04-28 09:07:16 -0700

    bugfix for callback_url issue in omniauth-oauth2 v1.4

--- lib/omniauth/strategies/rescuetime.rb
+++ lib/omniauth/strategies/rescuetime.rb
@@ -10,6 +10,11 @@ module OmniAuth
         :authorize_url => 'https://www.rescuetime.com/oauth/authorize',
         :response_type => 'code'
       }
+      # Bugfix for regression introduced after omniauth-oauth2 v1.3.1
+      # details: https://github.com/intridea/omniauth-oauth2/issues/8
+      def callback_url
+        options[:callback_url] || (full_host + script_name + callback_path)
+      end
     end
   end
 end
